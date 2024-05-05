3407bede225b11e325a91a59eea80bdf36f6d8a4
David Ferrari <david.ferrari@nubank.com.br>
2024-05-05 16:45:26 -0300

    adicionando lazygit direto no neovim

--- lua/redfoggg/plugins/general-plugins.lua
+++ lua/redfoggg/plugins/general-plugins.lua
@@ -52,4 +52,23 @@ return {
         "lewis6991/gitsigns.nvim",
         dependencies = { "nvim-lua/plenary.nvim" }
     },
+    {
+        "kdheepak/lazygit.nvim",
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        -- setting the keybinding for LazyGit with 'keys' is recommended in
+        -- order to load the plugin when the command is run for the first time
+        keys = {
+            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+        }
+    },
 }
