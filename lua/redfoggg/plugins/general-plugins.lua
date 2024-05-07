e956fd2829f9dfaa7804e3652c27d49a6ff8358e
David Ferrari <david.ferrari@nubank.com.br>
2024-05-06 21:15:49 -0300

    neorg adicionado

--- lua/redfoggg/plugins/general-plugins.lua
+++ lua/redfoggg/plugins/general-plugins.lua
@@ -66,14 +66,23 @@ return {
             "LazyGitFilter",
             "LazyGitFilterCurrentFile",
         },
-        -- optional for floating window border decoration
         dependencies = {
             "nvim-lua/plenary.nvim",
         },
-        -- setting the keybinding for LazyGit with 'keys' is recommended in
-        -- order to load the plugin when the command is run for the first time
         keys = {
             { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
         }
+    },
+    {
+        "vhyrro/luarocks.nvim",
+        priority = 1000,
+        config = true,
+    },
+    {
+        "nvim-neorg/neorg",
+        dependencies = { "luarocks.nvim" },
+        lazy = false,
+        version = "*",
+        config = true,
     },
 }
