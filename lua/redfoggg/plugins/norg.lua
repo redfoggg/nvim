89b2c894467dfb92e414a3a39bd68565ea248c21
David Ferrari <davidchristenes13@gmail.com>
2024-07-18 00:13:34 -0300

    retirada função específica para macos

--- lua/redfoggg/plugins/norg.lua
+++ lua/redfoggg/plugins/norg.lua
@@ -9,31 +9,6 @@ return {
         dependencies = { "luarocks.nvim" },
         lazy = false,
         version = "*",
-        build = function() -- essa etapa de build desta forma só é necessária no MacOS
-            local shell = require 'nvim-treesitter.shell_command_selectors'
-            local install = require 'nvim-treesitter.install'
-
-            -- save the original functions
-            local select_executable = shell.select_executable
-            local compilers = install.compilers
-
-            -- temporarily patch treesitter install logic
-            local cc = 'clang++ -std=c++11'
-            function shell.select_executable(executables)
-                return vim.tbl_filter(function(c) ---@param c string
-                    return c ~= vim.NIL and (vim.fn.executable(c) == 1 or c == cc)
-                end, executables)[1]
-            end
-
-            install.compilers = { cc }
-
-            -- install norg parsers
-            install.commands.TSInstallSync['run!'] 'norg' -- or vim.cmd [[ :TSInstallSync! norg ]]
-
-            -- restore the defaults back
-            shell.select_executable = select_executable
-            install.compilers = compilers
-        end,
         config = function()
             require("neorg").setup({
                 load = {
