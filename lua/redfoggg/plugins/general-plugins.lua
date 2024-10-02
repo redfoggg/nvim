99f6ace2ffc93d0b7c1c0444b889b9d7de41c4e1
David Ferrari <david.ferrari@david.ferrari>
2024-10-01 23:30:22 -0300

    removendo text

--- lua/redfoggg/plugins/general-plugins.lua
+++ lua/redfoggg/plugins/general-plugins.lua
@@ -114,11 +114,7 @@ return {
     {
         "folke/which-key.nvim",
         event = "VeryLazy",
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        },
+        opts = {},
         keys = {
             {
                 "<leader>?",
