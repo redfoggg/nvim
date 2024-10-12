a9bfc94260fe5a2901d3cec26f68c0ada0ffdaec
David Ferrari <davidchristenes13@gmail.com>
2024-10-12 13:17:38 -0300

    desabilitando previewers para find_files e git_files

--- lua/redfoggg/plugins/telescope.lua
+++ lua/redfoggg/plugins/telescope.lua
@@ -1,6 +1,18 @@
 return {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        require "telescope".setup {
+            pickers = {
+                find_files = {
+                    previewer = false
+                },
+                git_files = {
+                    previewer = false
+                }
+            }
+        }
+    end,
     keys = {
         { "<C-p>",       function() require('telescope.builtin').find_files() end },
         { "<leader>pf",  function() require('telescope.builtin').git_files() end },
