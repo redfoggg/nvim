a1b2a880a8595991b4e72b577192f26ded25b26a
David Ferrari <davidchristenes13@gmail.com>
2025-04-12 22:23:33 -0300

    ignorando pastas de build no telescope

--- lua/redfoggg/plugins/telescope.lua
+++ lua/redfoggg/plugins/telescope.lua
@@ -2,13 +2,26 @@ return {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
+        local ignore_patterns = {
+            "deps/.*",
+            "_build/.*",
+            ".elixir_ls/.*",
+            "assets/node_modules/.*",
+            "priv/static/.*",
+            "cover/.*"
+        }
         require "telescope".setup {
             pickers = {
                 find_files = {
-                    previewer = false
+                    previewer = false,
+                    file_ignore_patterns = ignore_patterns
                 },
                 git_files = {
-                    previewer = false
+                    previewer = false,
+                    file_ignore_patterns = ignore_patterns
+                },
+                live_grep = {
+                    file_ignore_patterns = ignore_patterns
                 }
             }
         }
