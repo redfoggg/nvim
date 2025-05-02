e4e820efeb4ad367b0d243359ae56924da633d94
David Ferrari <davidchristenes13@gmail.com>
2025-05-02 03:04:29 -0300

    removendo lsp-zero finalmente

--- lua/redfoggg/plugins/lsp.lua
+++ lua/redfoggg/plugins/lsp.lua
@@ -1,11 +1,9 @@
 return {
     "Hoffs/omnisharp-extended-lsp.nvim",
     {
-        "VonHeikemen/lsp-zero.nvim",
-        branch = "v3.x",
+        'neovim/nvim-lspconfig',
         dependencies = {
             -- LSP Support
-            'neovim/nvim-lspconfig',
             'williamboman/mason.nvim',
             'williamboman/mason-lspconfig.nvim',
 
@@ -25,27 +23,7 @@ return {
             "onsails/lspkind.nvim",
         },
         config = function()
-            local lsp_zero = require("lsp-zero")
             local lspkind = require("lspkind")
-
-            lsp_zero.preset("recommended")
-
-            -- inlayhints para lsp que suportam
-            -- vim.api.nvim_create_autocmd("LspAttach", {
-            --     group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-            --     callback = function(args)
-            --         local client = vim.lsp.get_client_by_id(args.data.client_id)
-            --         if client ~= nil and client.server_capabilities.inlayHintProvider then
-            --             vim.lsp.inlay_hint.enable(args.buf, true)
-            --         end
-            --     end
-            -- })
-
-            lsp_zero.set_server_config({
-                on_init = function(client)
-                    client.server_capabilities.semanticTokensProvider = nil
-                end,
-            })
 
             local dotnetConfig = {
                 handlers = {
@@ -56,7 +34,7 @@ return {
 
             local cmp = require('cmp')
             local cmp_select = { behavior = cmp.SelectBehavior.Insert }
-            local cmp_mappings = lsp_zero.defaults.cmp_mappings({
+            local cmp_mappings = {
                 ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                 ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
                 ['<C-y>'] = cmp.mapping(
@@ -66,9 +44,9 @@ return {
                     },
                     { "i", "c" }),
                 ["<C-Space>"] = cmp.mapping.complete(),
-            })
+            }
 
-            -- TODO essa parte precisa ser removida para
+            -- TODO: essa parte precisa ser removida para
             -- reorganizar o completion fora do local onde
             -- é organizado o lsp como está aqui tudo misturado
             cmp_mappings['<Tab>'] = nil
@@ -103,15 +81,15 @@ return {
                         [vim.diagnostic.severity.HINT] = '󰌵',
                     },
                 },
+                virtual_text = {
+                    prefix = ''
+                },
             })
 
-            lsp_zero.on_attach(function(client, bufnr)
+            local on_attach = function(client, bufnr)
+                client.server_capabilities.semanticTokensProvider = nil
                 local opts = { buffer = bufnr, remap = false }
 
-                -- por algum motivo é necessário chamar desta forma para que funcione
-                -- a config do handler não funciona com o lsp-zero como funcionava
-                -- com o lsp-config, preguiça de olhar o que é agora, quem sabe daqui a
-                -- alguns anos
                 if client.name == "omnisharp" then
                     vim.keymap.set("n", "gd", function() require('omnisharp_extended').lsp_definitions() end, opts)
                 else
@@ -128,16 +106,7 @@ return {
                 vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
                 vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
                 vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-            end)
-
-            lsp_zero.setup()
-
-            vim.diagnostic.config({ virtual_text = true })
-            vim.diagnostic.config {
-                virtual_text = {
-                    prefix = ''
-                },
-            }
+            end
 
             local ls = require "luasnip"
             ls.config.set_config {
@@ -156,7 +125,6 @@ return {
                 end
             end, { silent = true })
 
-            require('lspconfig')["dartls"].setup({})
             require('mason').setup({})
             require('mason-lspconfig').setup({
                 ensure_installed = {
@@ -167,14 +135,18 @@ return {
                 },
                 handlers = {
                     function(server_name)
-                        require('lspconfig')[server_name].setup({})
+                        require('lspconfig')[server_name].setup({
+                            on_attach = on_attach
+                        })
                         require('lspconfig')['hls'].setup {
+                            on_attach = on_attach,
                             filetypes = { 'haskell', 'lhaskell', 'cabal' },
                         }
                     end,
                     ["lua_ls"] = function()
                         local lspconfig = require("lspconfig")
                         lspconfig.lua_ls.setup {
+                            on_attach = on_attach,
                             settings = {
                                 Lua = {
                                     runtime = { version = "Lua 5.1" },
