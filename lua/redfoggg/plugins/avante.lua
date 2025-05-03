668c516768abb754e3a503d771a92b17e67f6e53
David Ferrari <davidchristenes13@gmail.com>
2025-05-02 21:20:33 -0300

    configuring copilot lua

--- lua/redfoggg/plugins/avante.lua
+++ lua/redfoggg/plugins/avante.lua
@@ -1,49 +1,72 @@
 return {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    version = false,
-    opts = {
-        provider = "copilot"
+    {
+        "zbirenbaum/copilot.lua",
+        config = function()
+            require('copilot').setup({
+                suggestion = {
+                    enabled = true,
+                    auto_trigger = true,
+                    debounce = 75,
+                },
+                panel = {
+                    enabled = true,
+                },
+                filetypes = {
+                    markdown = true,
+                    help = true,
+                }
+            })
+        end
     },
-    build = "make",
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-        "stevearc/dressing.nvim",
-        "nvim-lua/plenary.nvim",
-        "MunifTanjim/nui.nvim",
-        --- The below dependencies are optional,
-        "echasnovski/mini.pick",         -- for file_selector provider mini.pick
-        "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-        "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
-        "ibhagwan/fzf-lua",              -- for file_selector provider fzf
-        "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
-        "zbirenbaum/copilot.lua",        -- for providers='copilot'
-        {
-            -- support for image pasting
-            "HakonHarnes/img-clip.nvim",
-            event = "VeryLazy",
-            opts = {
-                -- recommended settings
-                default = {
-                    embed_image_as_base64 = false,
-                    prompt_for_file_name = false,
-                    drag_and_drop = {
-                        insert_mode = true,
+    {
+        "yetone/avante.nvim",
+        event = "VeryLazy",
+        version = false,
+        opts = {
+            provider = "ollama",
+            --provider = "copilot",
+            ollama = {
+                model = "qwen3:8b"
+            },
+        },
+        build = "make",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "stevearc/dressing.nvim",
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            --- The below dependencies are optional,
+            "echasnovski/mini.pick",     -- for file_selector provider mini.pick
+            "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+            "hrsh7th/nvim-cmp",          -- autocompletion for avante commands and mentions
+            "ibhagwan/fzf-lua",          -- for file_selector provider fzf
+            "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+            {
+                -- support for image pasting
+                "HakonHarnes/img-clip.nvim",
+                event = "VeryLazy",
+                opts = {
+                    -- recommended settings
+                    default = {
+                        embed_image_as_base64 = false,
+                        prompt_for_file_name = false,
+                        drag_and_drop = {
+                            insert_mode = true,
+                        },
                     },
                 },
             },
-        },
-        {
-            'MeanderingProgrammer/render-markdown.nvim',
-            opts = {
-                file_types = { "markdown", "Avante" },
-                render_modes = true,
-                sign = {
-                    enabled = false
+            {
+                'MeanderingProgrammer/render-markdown.nvim',
+                opts = {
+                    file_types = { "markdown", "Avante" },
+                    render_modes = true,
+                    sign = {
+                        enabled = false
+                    },
                 },
+                dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+                ft = { "markdown", "Avante" },
             },
-            dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
-            ft = { "markdown", "Avante" },
         },
-    },
-}
+    } }
